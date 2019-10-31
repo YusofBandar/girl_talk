c9557cb9411471b248b5c6f5cecc0b04b247a102
Yusof Bandar <ybandar1902@gmail.com>
2019-10-31 21:28:40 +0000

    Bunch of styling and positioning

--- visualisation/girl_talk.js
+++ visualisation/girl_talk.js
@@ -4,50 +4,57 @@ const trackPath = "../data/all_day/oh_no.json";
 const options = {
     width: 500,
     height: 500,
+    outerPadding: 10,
+    innerPadding: 100,
 }
 
 d3.json(trackPath).then((track) => {
 
-    const arcWidth = (options.width/2) / track.tracks.length;
+    const arcWidth = ((options.width-(options.outerPadding+options.innerPadding + 50)) / 2) / track.tracks.length;
     const duration = track.duration;
+
+
+    let colors = ["#9bd5f3", "#ecb60f", "#b75dc5", "#4dbd2c"]
 
     let svg = d3.select("body")
         .append("svg")
         .attr("width", options.width)
         .attr("height", options.height)
-        
+
 
     let record = svg.append("g")
-    
+        .style("transform", () => {
+            return `translate(250px,250px)`
+        })
+
 
     record.append("circle")
-        .attr("cx",options.width/2)
-        .attr("cy",options.height/2)
         .attr("r",options.width/2)
 
-  
+    record.append("circle")
+        .attr("r",options.innerPadding/2)
+        .style("fill","#909090")
 
     record.selectAll(".track")
         .data(track.tracks)
         .enter()
-        .append("g")
-        .style("transform",()=>{
-            return `translate(250px,250px)`
-        })
         .append("path")
-        .style("fill","white")
+        .style("fill", (d, i) => {
+
+            return colors[i % colors.length];
+        })
         .attr("d", (d, i) => {
             const circum = 2 * Math.PI;
             let startAngle = (d.startTime / duration) * circum;
             let endAngle = (d.endTime / duration) * circum;
 
             let path = d3.arc()
-                .innerRadius(250 - ((i+1) * arcWidth) )
-                .outerRadius(250 - (i * arcWidth))
+                .innerRadius(((options.width/2) - ((i + 1) * arcWidth)) - options.outerPadding)
+                .outerRadius(((options.width/2) - (i * arcWidth)) - options.outerPadding)
                 .startAngle(startAngle)
                 .endAngle(endAngle);
 
-                return path();
+            return path();
 
         })
 
