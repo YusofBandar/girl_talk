b36caa115eebe13d59ae8818a9132e4d0de8fd75
Yusof Bandar <ybandar1902@gmail.com>
2019-11-10 13:54:05 +0000

    Once album selected stays zoomed in

--- visualisation/album.js
+++ visualisation/album.js
@@ -2,7 +2,7 @@ class Album {
 
     constructor(albumPath) {
         this.albumPath = albumPath;
-
+        this.selected = false;
         this._init();
     }
 
@@ -12,6 +12,7 @@ class Album {
 
             const albumWidth = window.innerWidth < 450 ? 250 : 350;
 
+            let scope= this;
             let artWork = d3.select("body")
                 .append("img")
                 .attr("src", album.artWork)
@@ -19,30 +20,34 @@ class Album {
                 .style("height", `${albumWidth}px`)
                 .style("padding", "100px")
                 .on("mouseover", function () {
-                    d3.select(this)
+                    if(!scope.selected){
+                        d3.select(this)
                         .transition()
                         .duration(1000)
                         .style("width", `${albumWidth + 50}px`)
-                        .style("height", `${albumWidth + 50}px`)
+                        .style("height", `${albumWidth + 50}px`);
+                    }
                 })
                 .on("mouseout", function () {
-                    d3.select(this)
+                    console.log(scope.selected);
+                    if(!scope.selected){
+                        d3.select(this)
                         .transition()
                         .duration(1000)
                         .style("width", `${albumWidth}px`)
-                        .style("height", `${albumWidth}px`)
+                        .style("height", `${albumWidth}px`);
+                    }
                 })
                 .on("click", function () {
-
+                    scope.selected = true;
+                   
                     d3.select(this)
                         .transition()
                         .duration(800)
                         .ease(d3.easeExp)
-                        .style("width", `${window.innerWidth-100}px`)
-                        .style("height", `${window.innerWidth-100}px`)
-                        .style("padding", "0px")
-                        .style("position", "fixed")
-                        
+                        .style("width", `${window.innerWidth - 100}px`)
+                        .style("height", `${window.innerWidth - 100}px`)
+                        .style("padding", "0px");
                 })
 
         }).catch((err) => {
