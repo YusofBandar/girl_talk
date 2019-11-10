fdae0cbdbb1661cf5c0cfe7350aa528bcda1ef32
Yusof Bandar <ybandar1902@gmail.com>
2019-11-10 13:43:16 +0000

    Adds zoom in on click

--- visualisation/album.js
+++ visualisation/album.js
@@ -1,36 +1,48 @@
-class Album{
+class Album {
 
-    constructor(albumPath){
+    constructor(albumPath) {
         this.albumPath = albumPath;
-        
+
         this._init();
     }
 
-    _init(){
-        d3.json(this.albumPath).then((album) =>{
+    _init() {
+        d3.json(this.albumPath).then((album) => {
             this.album = album;
-            
+
             const albumWidth = window.innerWidth < 450 ? 250 : 350;
 
             let artWork = d3.select("body")
                 .append("img")
-                .attr("src",album.artWork)
-                .style("width",`${albumWidth}px`)
-                .style("height",`${albumWidth}px`)
-                .style("padding","100px")
-                .on("mouseover",function(){
+                .attr("src", album.artWork)
+                .style("width", `${albumWidth}px`)
+                .style("height", `${albumWidth}px`)
+                .style("padding", "100px")
+                .on("mouseover", function () {
                     d3.select(this)
                         .transition()
                         .duration(1000)
-                        .style("width",`${albumWidth + 50}px`)
-                        .style("height",`${albumWidth + 50}px`)
+                        .style("width", `${albumWidth + 50}px`)
+                        .style("height", `${albumWidth + 50}px`)
                 })
-                .on("mouseout",function(){
+                .on("mouseout", function () {
                     d3.select(this)
                         .transition()
                         .duration(1000)
-                        .style("width",`${albumWidth}px`)
-                        .style("height",`${albumWidth}px`)
+                        .style("width", `${albumWidth}px`)
+                        .style("height", `${albumWidth}px`)
+                })
+                .on("click", function () {
+
+                    d3.select(this)
+                        .transition()
+                        .duration(800)
+                        .ease(d3.easeExp)
+                        .style("width", `${window.innerWidth-100}px`)
+                        .style("height", `${window.innerWidth-100}px`)
+                        .style("padding", "0px")
+                        .style("position", "fixed")
+                        
                 })
 
         }).catch((err) => {
@@ -42,7 +54,7 @@ class Album{
 
 
 
-let albums = ["../data/all_day.json","../data/all_day.json"];
+let albums = ["../data/all_day.json"];
 
 albums.forEach(album => {
     new Album(album)
