9093d5599714c46b695dfe60a1a082153140e9c9
YusofBandar <ybandar1902@gmail.com>
2019-11-23 20:01:26 +0000

    Adds album component and reads in json

--- girl-talk/src/App.js
+++ girl-talk/src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
 
-import Record from "./components/record/record";
+import Album from "./components/album/album";
 
 class App extends Component {
 
-  albums = ["../../../data/all_day.json"];
+  albumPaths = ["./data/all_day.json"];
 
+  state = {
+    albums: []
+  }
+
+  async readJson(path) {
+    return await d3.json(path);
+  }
+
+  async componentWillMount() {
+    this.albumPaths.forEach(async (path) => {
+      let album = await this.readJson(path);
+      let albums = this.state.albums;
+      albums.push(album);
+      this.setState({ albums });
+    })
+  }
 
   render() {
     return (
       <div>
         <h1>Girl Talk</h1>
+        {
+          this.state.albums.map((album) => {
+            return <Album key={album.title} album={album}></Album>
+          })
+        }
       </div>
     );
   }
 }
 
 export default App;
+
+
---
+++ girl-talk/src/components/album/album.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import * as d3 from "d3";
+
+import './album.scss';
+
+import Record from "../record/record";
+
+class Album extends Component {
+
+    state = {
+        tracks: []
+    }
+
+    constructor(props) {
+        super(props);
+        this.album = this.props.album;
+    }
+
+    async readJson(path) {
+        return await d3.json(path);
+      }
+    
+      async componentWillMount() {
+        this.album.tracks.forEach(async (track) => {
+          let data = await this.readJson(track.dataPath);
+          let tracks = this.state.tracks;
+          tracks.push(data);
+          this.setState({ tracks });
+        })
+      }
+
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.tracks.map((track) => {
+                        return <h2>{track.track}</h2>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default Album;
