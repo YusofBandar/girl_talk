42b46d7be6943bf57743633e17a60c3cea403483
YusofBandar <ybandar1902@gmail.com>
2019-11-23 16:10:04 +0000

    Init track components

--- app/girl-talk/src/components/record/record.jsx
+++ app/girl-talk/src/components/record/record.jsx
@@ -2,34 +2,37 @@ import React, { Component } from 'react';
 
 import './record.scss';
 
+import Track from "../track/track";
+
 class Record extends Component {
 
     width = 500;
     height = 500;
-    innerPadding = this.width/15;
-    outerPadding = this.width/100;
-    radius = this.width/2;
+    innerPadding = this.width / 15;
+    outerPadding = this.width / 100;
+    radius = this.width / 2;
 
     constructor(props) {
         super(props);
         this.tracks = this.props.tracks || [];
         this.arcWidth = this.arcWidth.bind(this);
-      }
+    }
 
-    arcWidth(radius,innerPadding,outerPadding,numTracks){
+    arcWidth(radius, innerPadding, outerPadding, numTracks) {
         const arcWidth = (((radius * 2) - (outerPadding + innerPadding + 50)) / 2) / numTracks;
         return arcWidth.toFixed(2);
     }
 
     render() {
 
-        console.log(this.arcWidth(this.radius,this.innerPadding,this.outerPadding,this.tracks.length))
+        console.log(this.arcWidth(this.radius, this.innerPadding, this.outerPadding, this.tracks.length))
 
         return (
             <svg className="v-record" viewBox="0 0 1000 1000" width="1066" height="1066">
                 <g className="r-wrapper">
-                    <circle className="r-disk" r={this.width/2}></circle>
+                    <circle className="r-disk" r={this.width / 2}></circle>
                     <circle className="r-innerDisk" r={this.innerPadding}></circle>
+                    <Track></Track>
                 </g>
             </svg>
         );
---
+++ app/girl-talk/src/components/track/track.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './track.scss';
+
+class Track extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <g>
+            <text>TESTING</text>
+            </g>
+        );
+    }
+}
+
+export default Track;
